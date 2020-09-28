e92ba8c9ec1d0110686d18db538a81136dc5f9ce
rahul799 <rahul.agrawal@skuad.io>
2020-09-29 00:30:38 +0530

    improved script.js

--- AST/script.js
+++ AST/script.js
@@ -1,6 +1,11 @@
 var fs=require('fs');
-var data=fs.readFileSync('/home/rahul/Documents/GSOD/jsdoc_prototype/AST/generated.json', 'utf8');
+var data=fs.readFileSync('/home/rahul/Documents/GSOD/jsdoc_prototype/AST/AST.json', 'utf8');
 var words=JSON.parse(data);
+
+var fns = [];
+
+var candidate = {};
+var types = {};
 
 for(var i in words)
 {
@@ -11,8 +16,64 @@ for(var i in words)
     }
 }
 
+for(var i in words)
+{
+    if(words[i].kind == 'typedef')
+    {
+        types[words[i].name] = [];
+        for(var j in words[i].properties)
+        {
+            var val = {};
+            val.name = words[i].properties[j].name;
+            val.descr = words[i].properties[j].description;
+            types[words[i].name].push(val);
+        }
+    }
+}
+// console.log(types);
 
-fs.writeFile ("new_generated.json", JSON.stringify(words, null, 4), function(err) {
+for(var i in words)
+{
+    var func = {};
+    if(words[i].memberof != undefined)
+    {
+        func.name = words[i].longname;
+        func.descr = words[i].description;
+        func.formats = [];
+        
+        if(words[i].params != undefined)
+        {
+            var descr = words[i].params[0].description.split(" | ");
+            for( var j in descr)
+            {
+                var temp = {};
+
+                temp.args = [];
+                temp.descr = descr[j];
+
+                temp.args.push(types[words[i].params[0].type.names[0]][j]);
+                // console.log(temp);
+                func.formats.push(temp);
+            }
+        }
+        else
+        {
+           var val = words[i].tags.find(fn => fn.title == "param_desc");
+           var arr = {};
+           arr.descr = val.value;
+           func.formats.push(arr);
+        }
+        
+        fns.push(func);
+    }
+}
+
+var s = "Ajkdhcfhd dddf dih dscc dc  cde";
+var all = s.split(" | ");
+console.log(all[0]);
+
+// save generated file
+fs.writeFile ("new_generated.json", JSON.stringify(fns, null, 4), function(err) {
     if (err) throw err;
     console.log('complete');
 });
--- src/index.js
+++ src/index.js
@@ -1,50 +1,5 @@
 // @ts-check
 
-/**
- * a collection of one or more elements (nodes and edges)
- * @typedef {object} eles
- */
-
-/**
- * a collection of a single element (node or edge)
- * @typedef {object} ele
- */
-
-/**
- * a collection of one or more nodes
- * @typedef {object} nodes
- */
-
- /**
- * a collection of one or more nodes
- * @typedef {boolean} selector
- */
-
-/**
- * a collection of a single node
- * @typedef {object} node	
- */
-
-/**
- * a collection of one or more edges
- * @typedef {object} edges
- */
-
-/**
- * a collection of a single edge
- * @typedef {object} edge
- */
-
-/**
- * a layout
- * @typedef {object} layout
- */
-
-/**
- * an animation
- * @typedef {object} ani
- */
-
 var cytoscape = require('cytoscape');
 
 
@@ -55,20 +10,33 @@ var cy = cytoscape({
   container: document.getElementById('cy')
 });
 
+/**
+ * @typedef {object} cy_add
+ * @property {object} eleObj - A plain object that specifies the element.
+ * @property {object} eleObjs - An array of elements specified by plain objects.
+ * @property {object} eles - A collection of elements.
+ */
+
   /**
  * Add elements to the graph and return them
  * @memberof cy
- * @param { ele | eles } x - A plain object that specifies the element or A collection of elements.
+ * @param {...cy_add} x - Add a specified element to the graph. | Add the specified elements to the graph. | Add the specified elements to the graph.
  * @namespace cy.add
  */
 function add(x){
 
 }
 
+/**
+ * @typedef {object} cy_remove
+ * @property {object} eles -A collection of elements to remove.
+ * @property {object} selector - Elements matching this selector are removed.
+ */
+
   /**
  * Remove elements from the graph and return them.
  * @memberof cy
- * @param { eles | selector } x - A plain object that specifies the element or A collection of elements.
+ * @param {...cy_remove} x - Remove the specified elements. | Remove elements in the graph matching the specified selector.
  * @namespace cy.remove
  */
 function remove(x){
@@ -77,40 +45,41 @@ function remove(x){
 
   /**
  * Return a new, empty collection.
- * @memberof cy 
+ * @memberof cy
+ * @param_desc Get an empty collection.
  * @namespace cy.collection
  */
 function collection(){
 
 }
 
-  /**
- * Get an element from its ID in a very performant way
- * @memberof cy
- * @alias cy.$id
- * @param { ele } x - The ID of the element to get. 
- * @namespace cy.getElementById
- */
-function getElementById(x){
+//   /**
+//  * Get an element from its ID in a very performant way
+//  * @memberof cy
+//  * @alias cy.$id
+//  * @param { ele } x - The ID of the element to get. 
+//  * @namespace cy.getElementById
+//  */
+// function getElementById(x){
 
-}
+// }
 
-  /**
- * Get an element from its ID in a very performant way
- * @memberof cy
- * @param {function():any} callback - A callback within which you can make batch updates to elements.
- * @namespace cy.batch
- */
-function batch(callback){
+//   /**
+//  * Get an element from its ID in a very performant way
+//  * @memberof cy
+//  * @param {function():any} callback - A callback within which you can make batch updates to elements.
+//  * @namespace cy.batch
+//  */
+// function batch(callback){
 
-}
+// }
 
-  /**
- * Attaches the instance to the specified container for visualisation.
- * @memberof cy
- * @param { object } container - A HTML DOM element in which the graph should be rendered.
- * @namespace cy.mount
- */
-function mount(container){
+//   /**
+//  * Attaches the instance to the specified container for visualisation.
+//  * @memberof cy
+//  * @param { object } container - A HTML DOM element in which the graph should be rendered.
+//  * @namespace cy.mount
+//  */
+// function mount(container){
 
-}
+// }
